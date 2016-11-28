6b914970e3845c099096aea7bf4f86752375d821
Manuel <m.t.b.3@gmx.de>
2016-11-28 14:56:31 +0100

    Added pseudo random generated dice throw.

--- functions/randomNumber.js
+++ functions/randomNumber.js
@@ -6,8 +6,8 @@ module.exports = {
 
     // Returns a pseudo random number between 1 and including 6.
     getRandomDiceValue: function () {
-        min = 1;
-        max = 7;
+        var min = 1;
+        var max = 7;
         var randomNumber = Math.floor(Math.random() * (max - min) + min);
 
         // Make sure our random number is not the maximum value. If so decrease it by one.
---
+++ playingField/creation/generateField.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Manuel on 28.11.2016.
+ */
+
+// This functions will generate the playing field and return it.
+function getNewField() {
+    var fieldSize = getFieldSize();
+
+
+}
+
+// Returns the size of a playing field.
+function getFieldSize() {
+    return 128;
+}
+
+function getFirstField() {
+
+
+
+    var playingFieldArray = new array();
+}
