86d03a1e631eb1edce3ff3a451ea48461d04178f
Manuel <m.t.b.3@gmx.de>
2016-12-21 10:25:29 +0100

    Players class refactored.

--- class/player.js
+++ class/player.js
@@ -1,38 +1,62 @@
-module.exports = function (socket) {
-    this.socket = socket;
-    this.isReady = false;
-    this.id = socket.id;
-    this.name = null;
-    this.color = null;
-    this.lang = 'en';
+class Player {
 
-    this.position = 0;
+    constructor(socket) {
+        this.socket = socket;
+        this.isReady = false;
 
-    this.setPosition = function (position) {
-        return this.position = position
+        // TODO: Check if still necessary or direct usage of this.socket.id.
+        this.socketId = socket.id;
+        this.name = null;
+        this.color = null;
+        this.lang = 'en';
+        this.position = 0;
+    }
+
+    // Sets the players position.
+    setPosition = function (position) {
+        return this.position = position;
     };
 
-    this.addPosition = function (add) {
+    // Moves the players position ahead.
+    addPosition = function (add) {
         return this.position += add;
     };
 
-    this.subPosition = function (add) {
-        return this.position -= add;
+    // Moves the players position behind.
+    subPosition = function (remove) {
+        return this.position -= remove;
     };
 
-    this.getPosition = function () {
+    // Returns the current players position.
+    getPosition = function () {
         return this.position;
     };
 
-    this.getSocket = function () {
+    // Returns the socket.
+    getSocket = function () {
         return this.socket;
     };
 
-    this.getId = function () {
-        return this.id;
+    // Returns the socket id.
+    getId = function () {
+        return this.socketId;
     };
 
-    this.emit = function (event, data) {
+    isPlayerReady = function () {
+        return this.isReady;
+    };
+
+    setPlayerReady = function () {
+        this.isReady = true;
+    };
+
+    getPlayersLanguage = function () {
+        return this.lang;
+    };
+
+    emit = function (event, data) {
         this.socket.emit(event, data);
     };
-};
+}
+
+module.exports = Player;
