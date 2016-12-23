1c9dbf62fde96e32e1bd1e0be777a9326e5c68ee
theRealBaccata <m.t.b.3@gmx.de>
2016-12-24 00:50:28 +0100

    Allowed CORS.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 global.APPLICATION_PATH = __dirname;
 
-var Question = require('./class/question');
-var question = new Question();
-
-var test = question.getQuestionWithAnswers("History", "easy", "English");
-
-var test2 = "";
-/*global.APPLICATION_PATH = __dirname;
+// var Question = require('./class/question');
+// var question = new Question();
+//
+// var test = question.getQuestionWithAnswers("History", "easy", "English");
+//
+// var test2 = "";
+global.APPLICATION_PATH = __dirname;
  global.ROOM_COUNT = 0;
  global.VALIDATOR = require('./functions/validator/dataChecker');
  global.RANDOM_NUMBER = require('./functions/randomNumber');
@@ -14,6 +14,14 @@ var test2 = "";
  var Controller = require('./class/controller');
 
  const app = require('express')();
+
+ // Allow CORS.
+ app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
+
  const server = require('http').Server(app);
  const io = require('socket.io')(server);
 
@@ -30,4 +38,4 @@ var test2 = "";
  clients.push(socket);
  if (clients.length > 1)
  instances.push(new Controller(io, [clients.shift(), clients.shift()]));
- });*/
+ });
