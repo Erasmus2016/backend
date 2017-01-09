886dad8b45d62285b93a42e4848c5e3157a49d99
Manuel <m.t.b.3@gmx.de>
2017-01-09 08:32:54 +0100

    New year, new luck.

--- app.js
+++ app.js
@@ -1,71 +1,70 @@
 global.APPLICATION_PATH = __dirname;
 
-
 global.ROOM_COUNT = 0;
 global.VALIDATOR = require('./functions/validator/dataChecker');
 global.RANDOM_NUMBER = require('./functions/randomNumber');
 
 // START: Random dice throw testing.
-var dice;
-var diceThrows = 1000000000;
-var one = 0;
-var two = 0;
-var three = 0;
-var four = 0;
-var five = 0;
-var six = 0;
+// var dice;
+// var diceThrows = 1000000000;
+// var one = 0;
+// var two = 0;
+// var three = 0;
+// var four = 0;
+// var five = 0;
+// var six = 0;
+//
+// for (var i = 0; i < diceThrows; i++) {
+//     dice = RANDOM_NUMBER.getRandomDiceValue();
+//
+//     switch (dice) {
+//         case 1:
+//             one++;
+//             break;
+//         case 2:
+//             two++;
+//             break;
+//         case 3:
+//             three++;
+//             break;
+//         case 4:
+//             four++;
+//             break;
+//         case 5:
+//             five++;
+//             break;
+//         case 6:
+//             six++;
+//             break;
+//         default:
+//             throw "error";
+//     };
+// };
+//
+// console.log("1: " + one);
+// console.log("2: " + two);
+// console.log("3: " + three);
+// console.log("4: " + four);
+// console.log("5: " + five);
+// console.log("6: " + six);
+// // END: Random dice throw testing.
 
-for (var i = 0; i < diceThrows; i++) {
-    dice = RANDOM_NUMBER.getRandomDiceValue();
-
-    switch (dice) {
-        case 1:
-            one++;
-            break;
-        case 2:
-            two++;
-            break;
-        case 3:
-            three++;
-            break;
-        case 4:
-            four++;
-            break;
-        case 5:
-            five++;
-            break;
-        case 6:
-            six++;
-            break;
-        default:
-            throw "error";
-    };
-};
-
-console.log("1: " + one);
-console.log("2: " + two);
-console.log("3: " + three);
-console.log("4: " + four);
-console.log("5: " + five);
-console.log("6: " + six);
-// END: Random dice throw testing.
-
-let Question = require('./class/question');
-let question = new Question();
-
-const Database = require('./class/database');
-
-new Database(function (db) {
-    global.DB = db;
-});
-
-    question.getQuestionWithAnswers('History', 'easy', "English").then(function (result) {
-        console.log("Result from database call: " + result);
-        console.log(result[0]);
-        console.log(result[2]);
-    });
+// let Question = require('./class/question');
+// let question = new Question();
+//
+// const Database = require('./class/database');
+//
+// new Database(function (db) {
+//     global.DB = db;
 // });
-/**
+//
+//     question.getQuestionWithAnswers('History', 'easy', "English").then(function (result) {
+//         console.log("Result from database call: " + result);
+//         console.log(result[0]);
+//         console.log(result[2]);
+//     });
+// // });
+// /**
 
  const Question = require('./class/question');
  const question = new Question();
@@ -73,7 +72,7 @@ new Database(function (db) {
  let test = question.getQuestionWithAnswers("History", "easy", "English");
  //
  // var test2 = "";
- /**
+
 
 
  var Controller = require('./class/controller');
@@ -103,4 +102,4 @@ new Database(function (db) {
  clients.push(socket);
  if (clients.length > 1)
  instances.push(new Controller(io, [clients.shift(), clients.shift()]));
- });*/
+ });
