f8822485a65d539ab62fd7792629427b346aee91
Manuel <m.t.b.3@gmx.de>
2017-02-15 10:01:18 +0100

    Moved CORS handling to separate function.

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ const Database = require('./class/database'),
 
 new Database((db) => {
 
-    // Allow CORS.
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*.barmania.eu");
-        res.header('Access-Control-Allow-Methods', 'GET,POST');
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+    allowCors();
 
     let clients = [];
     const instances = {};
@@ -37,12 +31,13 @@ new Database((db) => {
             instances[controller.getId()] = controller;
 
             controller.on('disconnect', () => {
-                //todo: listen for new connection after client disconnects
+                //Todo: Listen for new connection after client disconnects.
                 if (controller.players.size() == 0) {
                     delete instances[controller.getId()];
                     log('room ' + controller.room_name + ' closed', 'red');
                 }
-            }).on('done', () => {//disconnect players and delete instance todo: play again -> push back to client queue
+                // Disconnect players and delete instance. Todo: Play again -> push back to client queue.
+            }).on('done', () => {
                 controller.players.each((player) => {
                     player.getSocket().disconnect();
                 });
@@ -54,3 +49,13 @@ new Database((db) => {
     server.listen(5000);
     console.log('server is running on port 5000');
 });
+
+// Allow CORS.
+function allowCors() {
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*.barmania.eu");
+        res.header('Access-Control-Allow-Methods', 'GET,POST');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+}
