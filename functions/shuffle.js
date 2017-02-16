93bd051be6413d8c4d3e988a4c3cd566359cf953
Manuel <m.t.b.3@gmx.de>
2017-02-16 12:25:47 +0100

    Improved some comments.

--- functions/shuffle.js
+++ functions/shuffle.js
@@ -2,21 +2,21 @@
  * Created by Manuel on 16.02.2017.
  */
 
-// Shuffles the input array and returns the result.
+// Shuffles the input array and returns it afterwards.
 function shuffle(array) {
     let counter = array.length;
     let index;
     let temp;
 
-    // While there are elements in the array
+    // While there are elements in the array.
     while (counter > 0) {
         // Pick a random index
         index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
+        // Decrease counter by 1.
         counter--;
 
-        // And swap the last element with it
+        // And swap the last element with it.
         temp = array[counter];
         array[counter] = array[index];
         array[index] = temp;
