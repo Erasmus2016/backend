a4c3caef21ffef8fcde37c90adf7fc5ffba51791
Manuel <m.t.b.3@gmx.de>
2017-02-16 12:12:19 +0100

    Question class refactoring.

--- class/question.js
+++ class/question.js
@@ -2,11 +2,18 @@
  * Created by Manuel on 01.12.2016.
  */
 
+const shuffle = require('../functions/shuffle');
+
 class Question {
 
     constructor(db) {
         this.db = db;
         this.usedQuestionIds = [];
+
+        this.category = '';
+        this.difficulty = '';
+        this.questionItem = '';
+        this.languageId = '';
     };
 
     // Gets the question with the appropriate answers from database and returns it.
@@ -17,71 +24,109 @@ class Question {
     // 2. The translated answers as a string array.
     getQuestionWithAnswers(category, difficulty, language) {
 
+        this.category = category;
+        this.difficulty = difficulty;
+
         try {
-            // Query database and get the language id.
-            // TODO: Make sure, the database languages entries are not going to change -> use a switch-case instead of an database query -> getLanguageId function.
-            const sql1 = 'SELECT id FROM language ' +
-                'WHERE language = "' + language + '"';
+            return this.setLanguageId(language).then(() => {
 
-            return this.db.query(sql1).then((result) => {
+                return this.determineQuestion().then(() => {
 
-                const languageId = result[0].id;
-                const difficultyInt = Question.getDifficulty(difficulty);
+                    if (this.isNewQuestion()) {
 
-                // Query database and get one random question.
-                const sql2 = 'SELECT * FROM question ' +
-                    'WHERE difficulty = ' + difficultyInt + ' ' +
-                    'AND category = "' + category + '" ' +
-                    'ORDER BY RAND() LIMIT 1';
+                        return this.getTranslatedQuestion().then((translatedQuestion) => {
 
-                return this.db.query(sql2).then((result) => {
+                            return this.getTranslatedAnswers().then((translatedAnswers) => {
 
-                    const question = result[0];
-                    const isNewQuestion = this.isNewQuestion(question.id);
-
-                    if (isNewQuestion) {
-
-                        // Query database and get the translated question.
-                        const sql3 = 'SELECT content FROM question_translation ' +
-                            'WHERE question_id = "' + question.short_code + '" ' +
-                            'AND language_id = ' + languageId;
-
-                        return this.db.query(sql3).then((result) => {
-
-                            const translatedQuestion = result[0].content;
-
-                            // Query database and get the translated answers.
-                            const sql4 = 'SELECT id, content FROM answer_translation ' +
-                                'WHERE question_id = "' + question.short_code + '" ' +
-                                'AND language_id = ' + languageId;
-
-                            return this.db.query(sql4).then((result) => {
-
-                                const translatedAnswers = result;
-
-                                return new Promise((resolve) => {
-                                    return resolve([question, translatedQuestion, translatedAnswers]);
-                                });
+                                return Promise.resolve([this.questionItem, translatedQuestion, translatedAnswers]);
                             });
                         });
                     }
                     else {
                         // For understanding recursion you first have to understand recursion.
-                        return new Promise((resolve) => {
-                            return resolve(this.getQuestionWithAnswers(category, difficulty, language));
-                        });
+                        return Promise.resolve(this.getQuestionWithAnswers(category, difficulty, language));
                     }
                 });
             });
         }
         catch (ex) {
             console.log(ex);
+            throw ex;
         }
     };
 
+    // Checks and returns true, if the question wasn't already used within this game - otherwise false.
+    isNewQuestion() {
+        const index = this.usedQuestionIds.indexOf(this.questionItem.id);
+
+        if (index === -1) {
+            this.saveQuestionIdToRam();
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    // Adds a question id to the already used question ids array.
+    saveQuestionIdToRam() {
+        this.usedQuestionIds.push(this.questionItem.id);
+    };
+
+    // Sets the language id (required for both question and answers).
+    setLanguageId(language) {
+        // Query database and get the language id.
+        // TODO: Make sure, the database languages entries are not going to change -> use a switch-case instead of an database query -> getLanguageId function.
+        const sql = 'SELECT id FROM language ' +
+            'WHERE language = "' + language + '"';
+
+        return this.db.query(sql).then((result) => {
+            this.languageId = result[0].id;
+        });
+    }
+
+    // Determines and sets a random question object based on the selected category and difficulty level.
+    determineQuestion() {
+        const difficultyInt = Question.getDifficultyId(this.difficulty);
+
+        // Query database and get one random question.
+        const sql = 'SELECT * FROM question ' +
+            'WHERE difficulty = ' + difficultyInt + ' ' +
+            'AND category = "' + this.category + '" ' +
+            'ORDER BY RAND() LIMIT 1';
+
+        return this.db.query(sql).then((result) => {
+            this.questionItem = result[0];
+        });
+    }
+
+    // Returns the translated question for this question.
+    getTranslatedQuestion() {
+        // Query database and get the translated question.
+        const sql = 'SELECT content FROM question_translation ' +
+            'WHERE question_id = "' + this.questionItem.short_code + '" ' +
+            'AND language_id = ' + this.languageId;
+
+        return this.db.query(sql).then((result) => {
+            return result[0].content;
+        });
+    }
+
+    // Returns the translated answers for this question as an array.
+    getTranslatedAnswers() {
+        // Query database and get the translated answers.
+        const sql = 'SELECT id, content FROM answer_translation ' +
+            'WHERE question_id = "' + this.questionItem.short_code + '" ' +
+            'AND language_id = ' + this.languageId;
+
+        return this.db.query(sql).then((result) => {
+            // Shuffle the answers to avoid repetition.
+            return shuffle.shuffleAnswers(result);
+        });
+    }
 
     // Returns the question difficulty as an integer.
-    static getDifficulty(difficulty) {
+    static getDifficultyId(difficulty) {
         switch (difficulty) {
             case "easy":
                 return 1;
@@ -106,102 +151,6 @@ class Question {
             default:
                 throw 'Unable to get languageId.';
         }
-    };
-
-    // Checks and returns true, if the question wasn't already used within this game - otherwise false.
-    isNewQuestion(questionId) {
-        const index = this.usedQuestionIds.indexOf(questionId);
-
-        if (index === -1) {
-            this.saveQuestionIdToRam(questionId);
-            return true;
-        }
-        else {
-            return false;
-        }
-    };
-
-    // Adds a question id to the already used question ids array.
-    saveQuestionIdToRam(questionId) {
-        this.usedQuestionIds.push(questionId);
-    };
-
-    // Returns the language id for both question and answer.
-    getLanguage(language) {
-
-        // Query database and return the language id.
-        const sql = 'SELECT id FROM language ' +
-            'WHERE language = ?';
-
-        this.connection.query(sql, [language], (error, result) => {
-            if (error) {
-                throw error;
-            }
-            else {
-                console.log('LanguageId: ', result);
-                return result;
-            }
-        });
-    };
-
-    // Returns a random question object based on input category and difficulty level.
-    getQuestion(category, difficulty) {
-
-        const difficultyInt = this.getDifficulty(difficulty);
-
-        // Query database and return one random question.
-        const sql = 'SELECT * FROM question ' +
-            'WHERE difficulty = ? ' +
-            'AND category = ? ' +
-            'ORDER BY RAND() LIMIT 1';
-
-        this.connection.query(sql, difficultyInt, category, (error, result) => {
-            if (!error) {
-                console.log('Question: ', result);
-                return result;
-            }
-            else {
-                throw error;
-            }
-        });
-    };
-
-    // Returns the translated question for this question.
-    getQuestionTranslation(question_short_code, languageId) {
-
-        // Query database and return the translated question.
-        const sql = 'SELECT content FROM question_translation ' +
-            'WHERE question_id = ? ' +
-            'AND language_id = ??';
-
-        this.connection.query(sql, question_short_code, languageId, (error, result) => {
-            if (!error) {
-                console.log('Translated question: ', result);
-                return result;
-            }
-            else {
-                throw error;
-            }
-        });
-    };
-
-    // Returns the translated answers for this question as an array.
-    getAnswersTranslation(question_short_code, languageId) {
-
-        // Query database and return the translated answers.
-        const sql = 'SELECT content FROM answer_translation ' +
-            'WHERE question_id = ? ' +
-            'AND language_id = ??';
-
-        this.connection.query(sql, question_short_code, languageId, (error, result) => {
-            if (!error) {
-                console.log('Translated answers: ', result);
-                return result;
-            }
-            else {
-                throw error;
-            }
-        });
     };
 }
 
