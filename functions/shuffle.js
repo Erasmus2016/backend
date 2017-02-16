c85ea65a595d0c1712e04b637a3ad6831a89bb50
Manuel <m.t.b.3@gmx.de>
2017-02-16 12:10:30 +0100

    Added shuffle function in order to shuffle the provided answers.

---
+++ functions/shuffle.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Manuel on 16.02.2017.
+ */
+
+// Shuffles the input array and returns the result.
+function shuffle(array) {
+    let counter = array.length;
+    let index;
+    let temp;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+module.exports = {
+    shuffleAnswers: shuffle,
+};
